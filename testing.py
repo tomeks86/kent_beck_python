72a57ea0e9bed9dc90441e0a9989c5fbe2476db5
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-04-10 10:18:44 +0200

    TestCase, WasRun implemented

---
+++ testing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+class TestCase:
+	def __init__(self, name):
+		self.name = name
+	def run(self):
+		method = getattr(self, self.name)
+		method()
+
+class WasRun(TestCase):
+	def __init__(self, name):
+		self.wasRun = None
+		TestCase.__init__(self, name)
+	def testMethod(self):
+		self.wasRun = 1
+
+
+test = WasRun("testMethod")
+print test.wasRun
+test.run()
+print test.wasRun
+
