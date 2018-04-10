47e728105cefb624a733284298f95dafdbdf8fd4
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-04-10 15:34:38 +0200

    setUp invokation before

--- testing.py
+++ testing.py
@@ -3,7 +3,10 @@
 class TestCase:
 	def __init__(self, name):
 		self.name = name
+	def setUp(self):
+		pass
 	def run(self):
+		self.setUp()
 		method = getattr(self, self.name)
 		method()
 
@@ -11,12 +14,23 @@ class WasRun(TestCase):
 	def __init__(self, name):
 		self.wasRun = None
 		TestCase.__init__(self, name)
+		#super().__init__(name)
 	def testMethod(self):
 		self.wasRun = 1
+	def setUp(self):
+		self.wasRun = None
+		self.wasSetUp = 1
 
+class TestCaseTest(TestCase):
+	def setUp(self):
+		self.test = WasRun("testMethod")
+	def testRunning(self):
+		assert(not self.test.wasRun)
+		self.test.run()
+		assert(self.test.wasRun)
+	def testSetUp(self):
+		self.test.run()
+		assert(self.test.wasSetUp)
 
-test = WasRun("testMethod")
-print test.wasRun
-test.run()
-print test.wasRun
-
+TestCaseTest("testSetUp").run()
+TestCaseTest("testRunning").run()
